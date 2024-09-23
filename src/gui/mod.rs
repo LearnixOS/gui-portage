69c039bd06316101f75f83a4ca811407c810dff9
Rekketstone <bromine1092@gmail.com>
2024-09-23 10:25:45 -0700

    oh well I'll figure it out later, somebody better help me wiht this stupid iced thing

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -1,11 +1,1 @@
-use iced::widget::{button, column, text, Column};
-
-impl Counter {
-    fn view(&self) -> Column<Message> {
-        column![
-            button("+").on_press(Message::Increment),
-            text(self.value),
-            button("-").on_press(Message::Decrement),
-        ]
-    }
-}
+use iced;
